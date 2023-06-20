015af8f50f731417aad462f6a4805d4a9fdb29ee
mohammedalothmani <mohammedalothmani2121@gmail.com>
2023-06-21 02:47:27 +0300

    done class course and do class student and def for inroll new courss

--- finall.py
+++ finall.py
@@ -10,5 +10,22 @@ class Course:
         self.course_name = course_name
         self.course_mark = course_mark
 
+class Student:
+    total_students = 0
+    student_list = []
+
+    def __init__(self,student_name,student_age,student_number):
+        self.student_id = uuid.uuid4()
+        self.student_name = student_name
+        self.student_age = student_age
+        self.student_number = student_number
+        self.courses_list = []
+        Student.total_students += 1
+        Student.student_list.append(self)
+
+    def enroll_new_course(self, course):
+        self.courses_list.append(course)
+
+
 
 
